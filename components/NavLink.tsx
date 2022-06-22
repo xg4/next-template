1dcd2d3d63321ad1d469d7f102cf8414111113b6
xg4 <xingor4@gmail.com>
2022-06-22 11:50:51 +0800

    feat: update NavLink component

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,37 +1,63 @@
 import { useRouter } from 'next/router'
-import { MouseEventHandler } from 'react'
+import React from 'react'
 
-export interface NavLinkProps {
+export interface NavLinkProps
+  extends Omit<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    'className' | 'style' | 'children' | 'href'
+  > {
   href: string
-  className?: (isActive: boolean) => string
-  style?: (isActive: boolean) => React.CSSProperties
-  children?: React.ReactNode
+  className?: string | ((props: { isActive: boolean }) => string)
+  style?:
+    | React.CSSProperties
+    | ((props: { isActive: boolean }) => React.CSSProperties)
+  children?:
+    | React.ReactNode
+    | ((props: { isActive: boolean }) => React.ReactNode)
 }
 
-export default function NavLink({
-  children,
-  href,
-  className,
-  style,
-}: NavLinkProps) {
-  const router = useRouter()
+const NavLink = React.forwardRef<HTMLAnchorElement, NavLinkProps>(
+  function NavLinkWithRef(
+    {
+      'aria-current': _ariaCurrent = 'page',
+      className: _className,
+      style: _style,
+      href,
+      children,
+      ...rest
+    },
+    ref
+  ) {
+    const router = useRouter()
 
-  const isActive = router.asPath === href
-  console.log(router.asPath, href, isActive)
+    const isActive = router.asPath === href
 
-  const handleClick: MouseEventHandler<HTMLAnchorElement> = (evt) => {
-    evt.preventDefault()
-    router.push(href)
+    const handleClick: React.MouseEventHandler<HTMLAnchorElement> = (evt) => {
+      evt.preventDefault()
+      router.push(href)
+    }
+
+    const className =
+      typeof _className === 'function' ? _className({ isActive }) : _className
+
+    const style = typeof _style === 'function' ? _style({ isActive }) : _style
+
+    const ariaCurrent = isActive ? _ariaCurrent : undefined
+
+    return (
+      <a
+        {...rest}
+        ref={ref}
+        aria-current={ariaCurrent}
+        href={href}
+        className={className}
+        onClick={handleClick}
+        style={style}
+      >
+        {typeof children === 'function' ? children({ isActive }) : children}
+      </a>
+    )
   }
+)
 
-  return (
-    <a
-      href={href}
-      className={className && className(isActive)}
-      onClick={handleClick}
-      style={style && style(isActive)}
-    >
-      {children}
-    </a>
-  )
-}
+export default NavLink
