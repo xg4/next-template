49391f2ff9de9adaf29ba4ee49cab64649401fbb
xg4 <xingor4@gmail.com>
2022-07-22 15:00:46 +0800

    fix: use forward ref, input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -10,7 +10,7 @@ const Input = forwardRef(
 
     return (
       <input
-        type="text"
+        ref={ref}
         className={classNames(
           'px-3 py-1.5 text-base',
           'm-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none',
